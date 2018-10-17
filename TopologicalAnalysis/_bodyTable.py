6915df91e4c04e65206446588c35452f2e5cf77a
Shirui816 <2361394+Shirui816@users.noreply.github.com>
2018-10-17 17:25:09 +0800

    rename body_hash variable inside body_hash function

--- TopologicalAnalysis/_bodyTable.py
+++ TopologicalAnalysis/_bodyTable.py
@@ -10,13 +10,13 @@ def body_hash(body):
     :param body: 1-D np array with n atoms, for bodies (-1 for not a body)
     :return:
     """
-    body_hash = {}
+    ret = {}
     print('Build body hash...')
     natoms = len(body)
     bodies = list(set(list(body)))
     bodies.remove(-1)
     idxes = np.arange(natoms)
     for b in bodies:
-        body_hash[b] = idxes[body == b]
+        ret[b] = idxes[body == b]
     print('Done.')
-    return body_hash
+    return ret
