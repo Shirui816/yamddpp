531ff1ff1183a7677acef89354b5eb5fb4208067
Shirui816 <2361394+Shirui816@users.noreply.github.com>
2018-10-18 20:49:32 +0800

    BUG Fix of _msd.py

--- Diffusion/__init__.py
+++ Diffusion/__init__.py
@@ -1,2 +1,30 @@
-from ._msd import msd
+import numpy as np
 from ._msd import msd_square
+from simpletraj.dcd.dcd import DCDReader  # or self-made dcd reader
+from AutoCorrelation import vec_ac
+from ._msd import msd
+
+_BUFFER_SIZE = 200000000  # 2e8 coordinates for n_frames * n_particles
+
+
+def msd_dcd(dcd_file):
+    r"""
+    :param dcd_file: str, dcd_file
+    :return: np.ndarray, msd
+    """
+    dcd = DCDReader(dcd_file)
+    if dcd.periodic:
+        raise ValueError("Error, periodic data found!")
+    n_samples = dcd.numatoms
+    n_frames = dcd.numatoms
+    _BUFFER = _BUFFER_SIZE // n_frames
+    n_buffer = n_samples // _BUFFER
+    ret = np.zeros((n_frames,))
+    counter = 0
+    for i in range(n_buffer):
+        x = np.asarray([np.copy(_[counter:counter+_BUFFER]) for _ in dcd])
+        counter += _BUFFER
+        ret += (msd_square(x) - 2 * vec_ac(x, cum=True))
+    x = np.asarray([np.copy(_[counter:]) for _ in dcd])
+    ret += (msd_square(x) - 2 * vec_ac(x, cum=True))
+    return ret / n_samples
--- Diffusion/_msd.py
+++ Diffusion/_msd.py
@@ -1,44 +1,30 @@
 import numpy as np
-from simpletraj.dcd.dcd import DCDReader  # or self-made dcd reader
 from AutoCorrelation import vec_ac
 
-_BUFFER_SIZE = 200000000  # 2e8 coordinates for n_frames * n_particles
 
-
-def msd_square(x):
+def msd_square(x, cum=True):
     r"""
     :param x: np.ndarray, all vectors (n_frames, n_particles, n_dimension)
+    :param cum: bool, summing n_particles or not
     :return: np.ndarray, msd values
     """
-    n = x.shape[0]
-    xt = np.square(x).sum(axis=(1, 2))
-    xt = np.append(xt, 0)
-    x0 = 2 * xt.sum()
-    xm = np.zeros(n)
-    for m in range(n):
+    n, n_samples = x.shape[0], x.shape[1]
+    summing_axes = (1, 2) if cum else 2
+    xt = np.square(x).sum(axis=summing_axes)
+    x0 = 2 * xt.sum(axis=0)
+    _shape = (n, ) if cum else (n, n_samples)
+    xm = np.zeros(_shape)
+    xm[0] = x0 / n
+    for m in range(1, n):
         x0 = x0 - xt[m - 1] - xt[n - m]
-        xm[m] = xt / (n - m)
+        xm[m] = x0 / (n - m)
     return xm
 
 
-def msd(dcd_file):
+def msd(x, cum=True):
     r"""
-    :param dcd_file: str, dcd_file
+    :param x: np.ndarray, (n_frames, n_particles, n_dim)
+    :param cum: bool, summing n_particles or not.
     :return: np.ndarray, msd
     """
-    dcd = DCDReader(dcd_file)
-    if dcd.periodic:
-        raise ValueError("Error, periodic data found!")
-    n_samples = dcd.numatoms
-    n_frames = dcd.numatoms
-    _BUFFER = _BUFFER_SIZE // n_frames
-    n_buffer = n_samples // _BUFFER
-    ret = np.zeros((n_frames,))
-    counter = 0
-    for i in range(n_buffer):
-        x = np.asarray([np.copy(_[counter:counter+_BUFFER]) for _ in dcd])
-        counter += _BUFFER
-        ret += (msd_square(x) - 2 * vec_ac(x, cum=True))
-    x = np.asarray([np.copy(_[counter:]) for _ in dcd])
-    ret += (msd_square(x) - 2 * vec_ac(x, cum=True))
-    return ret / n_samples
+    return msd_square(x, cum=cum) - 2 * vec_ac(x, cum=cum)
