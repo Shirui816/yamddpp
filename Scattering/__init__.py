5c974ef12b14ff2419b3db74351b34f8c6efe400
Shirui816 <2361394+Shirui816@users.noreply.github.com>
2018-10-18 22:45:42 +0800

    Add rdf

--- Scattering/__init__.py
+++ Scattering/__init__.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: shirui <shirui816@gmail.com>
+
+from ._rdf import rdf
+from ._scatter import scatter
--- Scattering/_rdf.py
+++ Scattering/_rdf.py
@@ -6,7 +6,7 @@ import numpy as np
 from .cython_ import xyz_to_r
 
 
-def rdf(x, y, box, bins, r_bin=0.2):
+def rdf(x, y, box, bins, r_bin=0.2, mode='ab'):
     px, ex = np.histogramdd(x, bins=bins,
                             range=[(-_/2, _/2) for _ in box])
     py, ex = np.histogramdd(y, bins=bins,
@@ -16,7 +16,7 @@ def rdf(x, y, box, bins, r_bin=0.2):
     _ft_px_py = _ft_px * _ft_py.conj()
     _rdf_xyz = np.fft.irfftn(_ft_px_py)
     # _ft_py_px[t] == _ft_px_py[-t]
-    _rdf_xyz[0, 0, 0] = 0
+    _rdf_xyz[0, 0, 0] -= 0 if mode == 'ab' else x.shape[0]
     _rdf_xyz = np.fft.fftshift(_rdf_xyz)  # for x, y are in (-box/2, box/2)
     _r = np.vstack([_[:-1] + 0.5 * (_[-1] - _[-2]) for _ in ex])
     _rdf = xyz_to_r(_rdf_xyz, _r, box.min()/2, r_bin)
