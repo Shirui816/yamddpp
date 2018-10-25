add4faa6688a263e513b8a0d6a00b247a19eb9d7
Shirui816 <2361394+Shirui816@users.noreply.github.com>
2018-10-25 12:26:35 +0800

    Optimize vec_ac

--- AutoCorrelation/_mat_ac.py
+++ AutoCorrelation/_mat_ac.py
@@ -3,14 +3,15 @@ import numpy as np
 
 def vec_ac(x, cum=True):
     r"""Vector autocorrelation function with samples.
-    :param x: np.ndarray -> (n_frames, n_vectors, n_dimension)
+    :param x: np.ndarray -> (n_frames, n_vectors, ...)
     :param cum: bool, take average of n_vectors or not.
     :return: np.ndarray -> (n_frames,) of vector autocorrelation
             or (n_frames, n_vectors) if `cum=False'.
     """
     n = x.shape[0]
     ft_x = np.fft.rfft(x, axis=0, n=n * 2)  # FFT over time axis
-    summing_axes = (1, 2) if cum else 2
+    summing_axes = tuple(range(1, x.ndim)) if cum else \
+        tuple(range(2, x.ndim))
     ft_corr = np.sum(abs(ft_x) ** 2, axis=summing_axes)
     # summing over samples and dimension or just dimension
     return np.fft.irfft(ft_corr)[:n].real / np.arange(n, 0, -1)
