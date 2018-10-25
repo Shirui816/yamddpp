e990b6d2cedad7c0a07a594863cc7a6ba4a8f0a6
Shirui816 <2361394+Shirui816@users.noreply.github.com>
2018-10-25 12:11:41 +0800

    Fix bug in mat_ac

--- AutoCorrelation/_mat_ac.py
+++ AutoCorrelation/_mat_ac.py
@@ -6,6 +6,7 @@ def vec_ac(x, cum=True):
     :param x: np.ndarray -> (n_frames, n_vectors, n_dimension)
     :param cum: bool, take average of n_vectors or not.
     :return: np.ndarray -> (n_frames,) of vector autocorrelation
+            or (n_frames, n_vectors) if `cum=False'.
     """
     n = x.shape[0]
     ft_x = np.fft.rfft(x, axis=0, n=n * 2)  # FFT over time axis
@@ -22,5 +23,6 @@ def mat_ac(x):
     """
     n = x.shape[0]
     ft_x = np.fft.rfft(x, axis=0, n=n * 2)  # FFT over time axis
-    ft_corr = np.sum(abs(ft_x) ** 2)
+    ft_corr = np.sum(abs(ft_x) ** 2, axis=tuple(range(1, x.ndim)))
+    # summing over all axes except time axis.
     return np.fft.irfft(ft_corr)[:n].real / np.arange(n, 0, -1)
