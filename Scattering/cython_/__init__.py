6de299405ead81ab6e6a4f2978bc8fbabe5b4b49
Shirui816 <2361394+Shirui816@users.noreply.github.com>
2018-10-26 19:19:33 +0800

    F**k pycharm and 2 blank lines!

--- Scattering/cython_/__init__.py
+++ Scattering/cython_/__init__.py
@@ -6,6 +6,7 @@ from .functions import real_input
 from .functions import complex_input
 import numpy as np
 
+
 def hist_xyz_to_r(m_xyz, r, r_max, r_bin):
     if m_xyz.dtype == np.float64:
         return real_input(m_xyz, r, r_max, r_bin)
