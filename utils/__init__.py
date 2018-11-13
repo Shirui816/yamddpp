0c1d9359cd72d67337b9114d8cb8bffa31f6e4dc
Shirui816 <2361394+Shirui816@users.noreply.github.com>
2018-11-14 01:48:38 +0800

    histogram ndarray by length of indices, all-dimension version, python & numba.jit

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from ._hist_vec_to_mod import norm_vec_to_r
---
+++ utils/_hist_vec_to_mod.py
@@ -0,0 +1,21 @@
+from numba import jit
+import numpy as np
+
+
+@jit
+def norm_vec_to_r(x, r, r_bin, r_max):
+    r_max2 = r_max ** 2
+    ret = np.zeros(int(r_max / r_bin) + 1, dtype=np.float)
+    cter = np.zeros(ret.shape, dtype=np.float)
+    for idx in np.ndindex(x.shape):
+        rr = 0
+        for j, jdx in enumerate(idx):
+            rr += r[j, jdx] ** 2
+        if rr < r_max2:
+            kdx = int(rr ** 0.5 / r_bin)
+            ret[kdx] += x[idx]
+            cter[kdx] += 1
+    cter[cter == 0] = 1
+    return ret / cter
+
+# for datas of all dimensions. x.shape == r.shape == (500, 500, 500), ~ 2.05s
