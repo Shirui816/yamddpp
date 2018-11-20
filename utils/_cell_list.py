9de7588abbb60900e261fe19737771ca7ae18ee3
Shirui816 <2361394+Shirui816@users.noreply.github.com>
2018-11-20 22:24:09 +0800

    minor fix

--- utils/_cell_list.py
+++ utils/_cell_list.py
@@ -4,13 +4,13 @@ from numpy.core.multiarray import ndarray
 
 
 @jit
-def cell_id(x: np.ndarray, box: np.ndarray, ibox: np.ndarray) -> int:
+def cell_id(x: ndarray, box: ndarray, ibox: ndarray) -> int:
     ind: ndarray = np.asarray(((x / box + 0.5) * ibox), dtype=np.int64)
     return ind[0] + ind[1] * ibox[0] + ind[2] * ibox[1] * ibox[0]
 
 
 @jit
-def i_cell(cid: np.ndarray, ibox: np.ndarray) -> int:  # ravel in Fortran way.
+def i_cell(cid: ndarray, ibox: ndarray) -> int:  # ravel in Fortran way.
     ind: ndarray = np.asarray((cid + ibox) % ibox, dtype=np.int64)
     return ind[0] + ind[1] * ibox[0] + ind[2] * ibox[1] * ibox[0]
 
@@ -19,7 +19,7 @@ def i_cell(cid: np.ndarray, ibox: np.ndarray) -> int:  # ravel in Fortran way.
 
 
 @jit
-def cell_neighbours(ic: np.ndarray, ibox: np.ndarray) -> np.ndarray:
+def cell_neighbours(ic: ndarray, ibox: ndarray) -> ndarray:
     ret: ndarray = np.zeros(3 ** ibox.shape[0], dtype=np.int64)
     for i, ind in enumerate(np.ndindex((3,) * ibox.shape[0])):
         # iterator of unraveled n-dimensional indices.
@@ -29,7 +29,7 @@ def cell_neighbours(ic: np.ndarray, ibox: np.ndarray) -> np.ndarray:
 
 
 @jit
-def box_map(box: np.ndarray, r_cut: "np.ndarray or float") -> tuple:
+def box_map(box: ndarray, r_cut: "ndarray or float") -> tuple:
     ibox: ndarray = np.asarray(box / r_cut, dtype=np.int64)
     ret: ndarray = np.zeros((np.multiply.reduce(ibox), 3 ** box.shape[0]))
     for ind in np.ndindex(ibox):
@@ -40,7 +40,7 @@ def box_map(box: np.ndarray, r_cut: "np.ndarray or float") -> tuple:
 
 
 @jit
-def linked_cl(pos: np.ndarray, box: np.ndarray, ibox: np.ndarray) -> tuple:
+def linked_cl(pos: ndarray, box: ndarray, ibox: ndarray) -> tuple:
     head: ndarray = np.zeros(np.multiply.reduce(ibox), dtype=np.int64) - 1
     body: ndarray = np.zeros(pos.shape[0], dtype=np.int64)
     for i in range(pos.shape[0]):
