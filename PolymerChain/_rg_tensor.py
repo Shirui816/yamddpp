a64996a21254d02df51c9d8de29b654c51c74c0a
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2019-05-10 01:39:04 +0800

    Update _rg_tensor.py

--- PolymerChain/_rg_tensor.py
+++ PolymerChain/_rg_tensor.py
@@ -8,14 +8,15 @@ def pbc(r, d):
 
 def batchRgTensor(samples, boxes):
     r"""Batch calculation of Rg Tensors.
-    :param samples: np.ndarray, (...,n_chains, n_monomers,n_dimensions)
-    :param boxes: np.ndarray, (...,n_dimensions)
+    :param samples: np.ndarray, (...,n_chains, n_monomers,n_dim) e.g. (n_batch, n_frames, n_chains, n..., n_dim)
+    :param boxes: np.ndarray, (...,n_dimensions), e.g. (n_batch, n_frames, n_dim)
     :return: np.ndarray ret.
     """
-    if samples.ndim <= 3:
-        raise ValueError("NO~~~Are you using multiple box values for 1 frame data?")
-    else:
-        boxes = np.expand_dims(np.expand_dims(boxes, -2), -3)
+    if samples.ndim == 2:
+        samples = np.expand_dims(samples, 0)
+    if samples.ndim < boxes.ndim + 2:
+        raise ValueError("NO~~~Are you using multiple box values for smaller dataset of samples?")
+    boxes = np.expand_dims(np.expand_dims(boxes, -2), -3)
     chain_length = samples.shape[-2]
     samples = pbc(np.diff(samples, axis=-2), boxes).cumsum(axis=-2)
     com = np.expand_dims(samples.sum(axis=-2)/chain_length, -2)
