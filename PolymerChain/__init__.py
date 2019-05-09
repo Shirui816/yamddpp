6d495e5cb1aca1c5ab018b3d0d67fb497d027f37
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2019-05-10 01:04:38 +0800

    Update __init__.py

--- PolymerChain/__init__.py
+++ PolymerChain/__init__.py
@@ -1,2 +1,22 @@
 from ._rouse_modes import normal_modes
 # TODO: normal mode relaxation, CReTA
+from numba import float64
+from numba import guvectorize
+
+
+@guvectorize([(float64[:, :], float64[:, :], float64[:, :])], '(n,p),(p,m)->(n,m)',
+             target='parallel')  # target='cpu','gpu'
+def batch_dot(a, b, ret):  # much more faster than np.tensordot or np.einsum
+    r"""Vectorized universal function.
+    :param a: np.ndarray, (...,N,P)
+    :param b: np.ndarray, (...,P,M)
+    axes will be assigned automatically to last 2 axes due to the signatures.
+    :param ret: np.ndarray, results. (...,N,M)
+    :return: np.ndarray ret.
+    """
+    for i in range(ret.shape[0]):
+        for j in range(ret.shape[1]):
+            tmp = 0.
+            for k in range(a.shape[1]):
+                tmp += a[i, k] * b[k, j]
+        ret[i, j] = tmp
