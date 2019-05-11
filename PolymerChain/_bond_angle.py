776d74266d56a57dd152910f7dbf34489c05e3ea
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2019-05-11 21:27:14 +0800

    Update _bond_angle.py

--- PolymerChain/_bond_angle.py
+++ PolymerChain/_bond_angle.py
@@ -1,6 +1,5 @@
-from numba import vectorize
-from numba import float64
 from . import bondVecs
+from . import batch_inner_prod
 import numpy as np
 
 def bondAngles(samples: np.ndarray, boxes: np.ndarray) -> np.ndarray:
@@ -11,10 +10,23 @@ def bondAngles(samples: np.ndarray, boxes: np.ndarray) -> np.ndarray:
     e.g. (n_batch, n_frames, n_dim)
     :return: np.ndarray ret.
     """
-    #bond_vecs = bondVecs(samples, boxes)
-    bond_vecs = np.random
+    bond_vecs = bondVecs(samples, boxes)
     prod = np.einsum(
-        '...ij,...ij->...i', bond_vecs[..., 1:, :], bond_vecs[..., :-1, :]
+        '...ij,...ij->...i', bond_vecs[..., :-1, :], bond_vecs[..., 1:, :]
     )
     norm = np.linalg.norm(bond_vecs, axis=-1)
-    return np.arccos(np.clip(prod / norm[..., 1:] / norm[..., :-1], -1, 1))
+    return np.arccos(np.clip(prod / norm[..., :-1] / norm[..., 1:], -1, 1))
+
+
+def bondAngles_guv(samples: np.ndarray, boxes: np.ndarray) -> np.ndarray:
+    r"""Batch calculation of Rg Tensors.
+    :param samples: np.ndarray, (...,n_chains, n_monomers,n_dim)
+    e.g. (n_batch, n_frames, n_chains, n..., n_dim)
+    :param boxes: np.ndarray, (...,n_dimensions),
+    e.g. (n_batch, n_frames, n_dim)
+    :return: np.ndarray ret.
+    This version should be faster. ^_^
+    """
+    bond_vecs = bondVecs(samples, boxes)
+    prod = batch_inner_prod(bond_vecs[..., :-1, :], bond_vecs[..., 1:, :])
+    return np.arccos(np.clip(prod, -1, 1))
