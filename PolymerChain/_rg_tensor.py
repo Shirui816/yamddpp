a6bf63cf63d40694de8081c0b11b402cdc53cd9f
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2019-05-11 20:11:02 +0800

    Update _rg_tensor.py

--- PolymerChain/_rg_tensor.py
+++ PolymerChain/_rg_tensor.py
@@ -1,6 +1,6 @@
 import numpy as np
 from . import batch_dot
-from Aggregation import pbc
+from . import bondVecs
 
 
 def batchRgTensor(samples: np.ndarray, boxes: np.ndarray) -> np.ndarray:
@@ -11,17 +11,7 @@ def batchRgTensor(samples: np.ndarray, boxes: np.ndarray) -> np.ndarray:
     e.g. (n_batch, n_frames, n_dim)
     :return: np.ndarray ret.
     """
-    if samples.ndim == 2:
-        samples = np.expand_dims(samples, 0)
-    if samples.ndim < boxes.ndim + 2:
-        raise ValueError(
-            "Are you using multiple box values for an 1-frame sample?"
-        )
-    boxes = np.expand_dims(np.expand_dims(boxes, -2), -3)
-    chain_length = samples.shape[-2]
-    samples = pbc(
-        np.diff(samples, axis=-2, prepend=samples[..., :1, :]), boxes
-    ).cumsum(axis=-2)
+    samples = bondVecs(samples, boxes).cumsum(axis=-2)
     samples -= np.expand_dims(samples.mean(axis=-2), -2)
-    rgTensors = batch_dot(np.swapaxes(samples, -2, -1), samples) / chain_length
-    return rgTensors
+    rgTensors = batch_dot(np.swapaxes(samples, -2, -1), samples)
+    return rgTensors / samples.shape[-2]
