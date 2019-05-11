f9582d6f70c9d1d94048e2c353e4ee04825910d3
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2019-05-11 17:17:15 +0800

    Update _rg_tensor.py
    use `prepend` parameter of `np.diff` to avoid `np.append`

--- PolymerChain/_rg_tensor.py
+++ PolymerChain/_rg_tensor.py
@@ -20,7 +20,7 @@ def batchRgTensor(samples: np.ndarray, boxes: np.ndarray) -> np.ndarray:
     boxes = np.expand_dims(np.expand_dims(boxes, -2), -3)
     chain_length = samples.shape[-2]
     samples = pbc(
-        np.diff(samples, axis=-2, prepend=samples[...,:1,:]), boxes
+        np.diff(samples, axis=-2, prepend=samples[..., :1, :]), boxes
     ).cumsum(axis=-2)
     samples -= np.expand_dims(samples.mean(axis=-2), -2)
     rgTensors = batch_dot(np.swapaxes(samples, -2, -1), samples) / chain_length
