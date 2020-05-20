45b43e4f472baf0f871e0742546130e89e491620
Shirui816 <2361394+Shirui816@users.noreply.github.com>
2020-05-21 02:56:30 +0800

    re-organize

--- lib/Diffusion/self_diffusion/_msd.py
+++ lib/Diffusion/self_diffusion/_msd.py
@@ -3,18 +3,20 @@ import numpy as np
 from lib.TimeCorrelation import vec_ac
 
 
-def msd_square(x, cum=True):
+def msd_square(x, cum=None):
     r"""
-    :param x: np.ndarray, all vectors (n_frames, n_particles, n_dimension)
+    :param x: np.ndarray, all vectors (n_frames, ..., n_dimension)
     :param cum: bool, summing n_particles or not
     :return: np.ndarray, msd values
     """
     n, n_samples = x.shape[0], x.shape[1]
-    summing_axes = (1, 2) if cum else 2
+    if cum is not None:
+        summing_axes = tuple((*cum, -1)) if -1 not in cum else tuple(cum)
+    else:
+        summing_axes = (-1,)  # only add dimension
     xt = np.square(x).sum(axis=summing_axes)
     x0 = 2 * xt.sum(axis=0)
-    _shape = (n,) if cum else x.shape[:-1]
-    xm = np.zeros(_shape)
+    xm = np.zeros(xt.shape)
     xm[0] = x0 / n
     for m in range(1, n):
         x0 = x0 - xt[m - 1] - xt[n - m]
@@ -22,7 +24,7 @@ def msd_square(x, cum=True):
     return xm
 
 
-def msd(x, cum=True):
+def msd(x, cum=None):
     r"""
     :param x: np.ndarray, (n_frames, n_particles, n_dim)
     :param cum: bool, summing n_particles or not.
