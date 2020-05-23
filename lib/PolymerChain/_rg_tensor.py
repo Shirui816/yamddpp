4cfa5e37c44c8e909d086b44a767b0e9a34c6d1e
Shirui816 <2361394+Shirui816@users.noreply.github.com>
2020-05-23 21:08:48 +0800

    RgTensor

--- lib/PolymerChain/_rg_tensor.py
+++ lib/PolymerChain/_rg_tensor.py
@@ -5,21 +5,21 @@ from . import bond_vecs_common
 from ..Aggregation import pbc
 
 
-def batch_rg_tensor(samples: np.ndarray, boxes: np.ndarray) -> np.ndarray:
+def batch_rg_tensor(x: np.ndarray, boxes: np.ndarray) -> np.ndarray:
     r"""Batch calculation of Rg Tensors.
-    :param samples: np.ndarray, (...,n_chains, n_monomers,n_dim)
+    :param x: np.ndarray, (...,n_chains, n_monomers,n_dim)
     e.g. (n_batch, n_frames, n_chains, n..., n_dim)
     :param boxes: np.ndarray, (...,n_dimensions),
     e.g. (n_batch, n_frames, n_dim)
     :return: np.ndarray ret.
     """
-    samples, boxes = bond_vecs_common(samples, boxes)
+    x, boxes = bond_vecs_common(x, boxes)
     bv = pbc(
-        np.diff(samples, axis=-2, prepend=samples[..., :1, :]), boxes
+        np.diff(x, axis=-2, prepend=x[..., :1, :]), boxes
     ).cumsum(axis=-2)
     ree = bv[..., -1, :]
     cm = np.expand_dims(bv.mean(axis=-2), -2)
     bv = bv - cm
-    cm = pbc(samples[..., :1, :] + cm, boxes)
+    cm = pbc(x[..., :1, :] + cm, boxes)
     rg_tensors = batch_dot(np.swapaxes(bv, -2, -1), bv)
     return rg_tensors / bv.shape[-2], cm, ree
